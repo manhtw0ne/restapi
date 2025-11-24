b0d0198ad6fd0ce74e3f72dd6937e39162526fd1
manhtw0ne <manh.tw0ne@gmail.com>
2025-11-24 08:19:31 +0700

    Completed the development for get api

--- src/main/java/in/bushansirgur/restapi/repository/ExpenseRepository.java
+++ src/main/java/in/bushansirgur/restapi/repository/ExpenseRepository.java
@@ -3,5 +3,8 @@ package in.bushansirgur.restapi.repository;
 import in.bushansirgur.restapi.entity.ExpenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
+    Optional<ExpenseEntity> findByExpenseId(String expenseId);
 }
---
+++ webapp/src/components/ExpenseList.tsx
@@ -0,0 +1,42 @@
+import type { Expense } from "../model/Expense";
+import CurrencyUtils from "../utils/CurrencyUtils";
+import DateUtils from "../utils/DateUtils";
+
+interface Props {
+    expenses: Expense[]
+}
+
+const ExpenseList = ({expenses}: Props ) => {
+    
+    return (
+    <div className="card">
+            <h5 className="card-header">
+                Expense 
+                <span className="float-end">Amount</span>
+            </h5>
+
+            <div className="card-body">
+                {expenses.map((expense) => (
+                    <div key={expense.expenseId}>
+                        <div className="d-flex justify-content-between border-bottom-1 p-3 text-dark">
+                            <div className="card-title m-0">
+                                <h5>{expense.name}</h5>
+                                <span className="fst-italic">
+                                    {DateUtils.formatDateString(expense.date)}
+                                </span>
+                                <div className="card-subtitle">
+                                    <span className="badge rounded-pill app-primary-bg-color">
+                                        {CurrencyUtils.formatToINR(expense.amount)}
+                                    </span>
+                                </div>
+                        </div>
+                    </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+    
+}
+
+export default ExpenseList;
